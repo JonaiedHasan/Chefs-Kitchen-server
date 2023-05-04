5e3f360d825ee018a293fda9ee07fdf533b0c56e
Alvihub <jonaiedhasan120@gmail.com>
2023-05-05 03:21:58 +0600

    receipe

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-
+const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.json({ hey: "something is running" });
 });
@@ -23,6 +23,6 @@ app.get("/recipeData", (req, res) => {
   res.send(recipeDetails);
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("server running");
 });
