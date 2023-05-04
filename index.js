e248632561380352d541f163fcdae988a1e8da0a
Alvihub <jonaiedhasan120@gmail.com>
2023-05-04 19:35:46 +0600

    add the file

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ hey: "something is running" });
+});
+const chefDetails = require("./data_file/chefs.json");
+const recipeDetails = require("./data_file/recipe.json");
+
+app.get("/chefData", (req, res) => {
+  res.send(chefDetails);
+});
+
+app.get("/chefData/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChef = chefDetails.find((chef) => chef.id === id);
+  res.send(singleChef);
+});
+
+app.get("/recipeData", (req, res) => {
+  res.send(recipeDetails);
+});
+
+app.listen(5000, () => {
+  console.log("server running");
+});
